6eaf03e60f90a297b24b3d9cd1edad022ba314e8
chLi <565353780@qq.com>
2022-08-28 17:20:58 +0800

    update tmp path

--- method_manage/Config/path.py
+++ method_manage/Config/path.py
@@ -5,7 +5,7 @@ import platform
 
 TMP_SAVE_FOLDER_PATH = ""
 if platform.system().lower() == 'windows':
-    TMP_SAVE_FOLDER_PATH = "D:/tmp/"
+    TMP_SAVE_FOLDER_PATH = "P:/tmp/"
 elif platform.system().lower() == 'linux':
-    TMP_SAVE_FOLDER_PATH = "/home/chli/tmp/"
+    TMP_SAVE_FOLDER_PATH = "/home/chli/public/tmp/"
 
